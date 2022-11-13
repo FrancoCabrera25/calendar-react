c8ed3e25cbcf6a26f29efb10518f10332f8a05e5
Franco Cabrera <francocabrera@MacBook-Air-de-Franco.local>
2022-11-13 20:57:04 -0300

    test

--- jest.setup.js
+++ jest.setup.js
@@ -12,7 +12,7 @@
 //     path: '.env.test'
 // });
 
-// Realizar el mock completo de las variables de entorno
-// jest.mock('./src/helpers/getEnvVariables', () => ({
-//     getEnvVariables: () => ({ ...process.env })
-// }));
+//Realizar el mock completo de las variables de entorno
+jest.mock('./src/helpers/getEnvVariables', () => ({
+    getEnvVariables: () => ({ ...process.env })
+}));
